69d3dd4227252f2cead46115fe2cd902769f3afd
Agustin <darkstone19@gmail.com>
2023-04-19 21:53:04 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*
+ * EJ 11 : Defina una función que convierte una cadena de caracteres, pasada por referencia, a mayúsculas, a excepción de la primera letra de cada palabra, que debe estar en minúsculas.
+ */
 int main() {
     printf("Hello, World!\n");
     return 0;
